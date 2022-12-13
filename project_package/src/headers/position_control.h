b7f046be287815e63249c61eeaab0e66cb418930
Taba2812 <gabriele.tabarellidefatis@gmail.com>
2022-12-13 16:24:18 +0100

    re-organized project

--- project_package/src/headers/position_control.h
+++ project_package/src/headers/position_control.h
@@ -2,13 +2,19 @@
 #define __POSITION_CONTROL_H__
 
 #include "ros/ros.h"
+#include "sensor_msgs/JointState.h"
+#include "std_msgs/Float64MultiArray.h"
 #include <Eigen/Dense>
-#include "joint_publisher.h"
 
 #define JOINTS 6
-#define QUEUE_SIZE 1000
-#define LOOP_RATE 1000.
+#define QUEUE_SIZE 1
+#define LOOP_RATE 100
 
-void get_position(const sensor_msgs::JointState::ConstPtr & msg);
+//'shoulder_pan_joint', 'shoulder_lift_joint', 'elbow_joint', 'wrist_1_joint', 'wrist_2_joint', 'wrist_3_joint'
+
+std::string joint_names[JOINTS] = {"elbow_joint", "shoulder_lift_joint", "shoulder_pan_joint", "wrist_1_joint", "wrist_2_joint", "wrist_3_joint"};
+double q[JOINTS] = {0,0,0,0,0,0};
+
+void get_position(const sensor_msgs::JointState::ConstPtr& js);
 
 #endif
