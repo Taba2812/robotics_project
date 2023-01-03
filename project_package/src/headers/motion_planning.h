23c4f00c6965cddc694f0edda05763247eaca8cf
Taba2812 <gabriele.tabarellidefatis@gmail.com>
2023-01-03 08:56:13 +0100

    Started implementation on Path class

--- project_package/src/headers/motion_planning.h
+++ project_package/src/headers/motion_planning.h
@@ -1,4 +1,35 @@
 #ifndef __MOTION_PLANNING_H__
 #define __MOTION_PLANNING_H__
 
+#include "inverse_kinematics.h"
+
+class Path{
+private:
+    JointConfiguration ic;
+    JointConfiguration fc;
+public:
+    Path();
+    Path(const JointConfiguration& jci, const JointConfiguration& jcf);
+    JointConfiguration get_ic() const;
+    JointConfiguration get_fc() const;
+};
+
+Path::Path(){
+    ic << Eigen::MatrixXd::Zero();
+    fc << Eigen::MatrixXd::Zero();
+}
+
+Path::Path(const JointConfiguration& jci, const JointConfiguration& jcf){
+    ic = jci;
+    fc = jcf;
+}
+
+JointConfiguration Path::get_ic() const{
+    return this -> ic;
+}
+
+JointConfiguration Path::get_fc() const{
+    return this -> fc;
+}
+
 #endif
