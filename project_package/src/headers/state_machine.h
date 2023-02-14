8751e5a13a33b6d3fbca970c8cdc67c43e5692a1
Taba2812 <gabriele.tabarellidefatis@gmail.com>
2023-02-14 15:38:33 +0100

    Added state mahine file

---
+++ project_package/src/headers/state_machine.h
@@ -0,0 +1,15 @@
+#ifndef __STATE_MACHINE_H__
+#define __STATE_MACHINE_H__
+
+#include "ur5.h"
+
+#pragma once
+
+//we could implement an actual state machine to make sure every procedure is followed in the proper order and without clashing
+
+class State {
+public:
+    //functions
+};
+
+#endif
