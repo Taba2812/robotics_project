e53c2edf35bfa583965319e28c9bc9719c82cf64
Taba2812 <gabriele.tabarellidefatis@gmail.com>
2023-02-15 16:10:41 +0100

    Added library

--- computer_vision/src/testing/catkin_ws/src/test_package/src/receiver.cpp
+++ computer_vision/src/testing/catkin_ws/src/test_package/src/receiver.cpp
@@ -1,9 +1,9 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
-#include "sensor_msgs/PointCloud2"
+#include "sensor_msgs/PointCloud2.h"
 
 void ros_callback(const sensor_msgs::PointCloud2 &point_cloud) {
-    std::cout << "received data " << point_cloud->data[0] << std::endl;
+    std::cout << "received data " << point_cloud.data[0] << std::endl;
 }
 
 int main (int argc, char **argv) {
