24d8841cc6437c0e3b4a9be0318522c054be6317
Dawwo20415 <davidemongera@hotmail.com>
2023-02-16 16:27:47 +0100

    Implemented code to take data from Zed2 Camera

--- computer_vision/src/testing/catkin_ws/src/test_package/src/receiver.cpp
+++ computer_vision/src/testing/catkin_ws/src/test_package/src/receiver.cpp
@@ -8,81 +8,166 @@
 #include <pcl-1.10/pcl/PCLPointCloud2.h>
 #include <pcl_conversions/pcl_conversions.h>
 
-//#include <opencv2/core.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
 #include <vector>
+#include <fstream>
+
+#define IMAGE_WIDTH 1280
+#define IMAGE_HEIGHT 720
 
 typedef pcl::PointCloud<pcl::PointXYZ> PTL_PointCloud;
+typedef pcl::PointCloud<pcl::PointXYZ>::Ptr PTL_PointCloudPtr;
 
-void save_to_file(const sensor_msgs::PointCloud2ConstPtr &point_cloud) {
+int i = 0;
 
+#pragma region Save&Loat to/from Binary File
+
+//! Write cv::Mat as binary
+/*!
+\param[out] ofs output file stream
+\param[in] out_mat mat to save
+*/
+bool writeMatBinary(std::ofstream& ofs, const cv::Mat& out_mat)
+{
+	if(!ofs.is_open()){
+		return false;
+	}
+	if(out_mat.empty()){
+		int s = 0;
+		ofs.write((const char*)(&s), sizeof(int));
+		return true;
+	}
+	int type = out_mat.type();
+	ofs.write((const char*)(&out_mat.rows), sizeof(int));
+	ofs.write((const char*)(&out_mat.cols), sizeof(int));
+	ofs.write((const char*)(&type), sizeof(int));
+	ofs.write((const char*)(out_mat.data), out_mat.elemSize() * out_mat.total());
+
+	return true;
 }
 
-void ros_callback(const sensor_msgs::PointCloud2ConstPtr &point_cloud) {
-    
-    pcl::PCLPointCloud2 pcl_pc2;
-    pcl_conversions::toPCL(*point_cloud,pcl_pc2);
-    pcl::PointCloud<pcl::PointXYZ>::Ptr temp_cloud(new pcl::PointCloud<pcl::PointXYZ>);
-    pcl::fromPCLPointCloud2(pcl_pc2,*temp_cloud);
 
-    std::cout << "Width: " << temp_cloud->width << " Height: " << temp_cloud->height << std::endl; 
-
-    for (auto point : temp_cloud->points) {
-        std::cout << "x: " << point.x << " y: " << point.y << " z: " << point.z << std::endl;
-    }
-
+//! Save cv::Mat as binary
+/*!
+\param[in] filename filaname to save
+\param[in] output cvmat to save
+*/
+bool SaveMatBinary(const std::string& filename, const cv::Mat& output){
+	std::ofstream ofs(filename, std::ios::binary);
+	return writeMatBinary(ofs, output);
 }
 
-std::vector<float> get_data(const sensor_msgs::PointCloud2ConstPtr &point_cloud, int row, int col) {
-    //cv::Vec4f result;
-    std::vector<float> result;
 
-    int start = (point_cloud->row_step * row) + (point_cloud->point_step * 4 * col);
+//! Read cv::Mat from binary
+/*!
+\param[in] ifs input file stream
+\param[out] in_mat mat to load
+*/
+bool readMatBinary(std::ifstream& ifs, cv::Mat& in_mat)
+{
+	if(!ifs.is_open()){
+		return false;
+	}
+	
+	int rows, cols, type;
+	ifs.read((char*)(&rows), sizeof(int));
+	if(rows==0){
+		return true;
+	}
+	ifs.read((char*)(&cols), sizeof(int));
+	ifs.read((char*)(&type), sizeof(int));
 
-    for (int c = 0; c < 4; c++) {
-        float value;
-        uint8_t byte_array[4];
-        for (int i = 0; i < 4; i++) {
-            byte_array[i] = point_cloud->data[start + c + i];
-        }
-        std::copy(reinterpret_cast<const char*>(&byte_array[0]),
-                  reinterpret_cast<const char*>(&byte_array[4]),
-                  reinterpret_cast<char*>(&value));
-        
-        result[c] = value;
-        std::cout << value << " | ";
-    }
+	in_mat.release();
+	in_mat.create(rows, cols, type);
+	ifs.read((char*)(in_mat.data), in_mat.elemSize() * in_mat.total());
 
-    std::cout << std::endl;
-
-    return result;
+	return true;
 }
 
-std::vector<std::vector<std::vector<float>>> pc2_to_Mat(const sensor_msgs::PointCloud2ConstPtr &point_cloud) {
-    //cv::Mat pcm(point_cloud->height, point_cloud->width, CV_32FC4, cv::Scalar(1));
-    std::vector<std::vector<std::vector<float>>> pcm;
 
-    for (int h = 0; h < point_cloud->height; h++) {
-        for (int w = 0; w < point_cloud->width; w++) {
+//! Load cv::Mat as binary
+/*!
+\param[in] filename filaname to load
+\param[out] output loaded cv::Mat
+*/
+bool LoadMatBinary(const std::string& filename, cv::Mat& output){
+	std::ifstream ifs(filename, std::ios::binary);
+	return readMatBinary(ifs, output);
+}
+#pragma endregion
 
-            int distance;
-            //pcm[h][w] = get_data(point_cloud, h, w);
+cv::Mat pcl_to_Mat(const PTL_PointCloudPtr point_cloud) {
+    cv::Mat pcm(IMAGE_HEIGHT, IMAGE_WIDTH, CV_32FC4, cv::Scalar(0));
+
+    for (int h = 0; h < IMAGE_HEIGHT; h++) {
+        for (int w = 0; w < IMAGE_WIDTH; w++) {
+            pcl::PointXYZ pcl_point = point_cloud->points[h + w];
+            float distance = sqrt(pow(pcl_point.x,2) + pow(pcl_point.y,2) + pow(pcl_point.z,2));
+            cv::Vec4f point(pcl_point.x, pcl_point.y, pcl_point.z, distance);
+            //std::cout << "Vx: " << point[0] << " Vy: " << point[1] << " Vz: " << point[2] << " Vd: " << point[3] << std::endl;
+            pcm.at<cv::Vec4f>(h,w) = point;
         }
     }
 
     return pcm;
 }
 
+void ros_callback(const sensor_msgs::PointCloud2ConstPtr &point_cloud) {
+    
+    std::cout << "Package Received" << std::endl;
+
+    pcl::PCLPointCloud2 pcl_pc2;
+    pcl_conversions::toPCL(*point_cloud,pcl_pc2);
+    PTL_PointCloudPtr temp_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::fromPCLPointCloud2(pcl_pc2,*temp_cloud);
+
+    std::cout << "Width: " << temp_cloud->width << " Height: " << temp_cloud->height << std::endl; 
+
+    cv::Mat new_cv_mat = pcl_to_Mat(temp_cloud);
+
+    //cv::Mat display_cloud;
+    //cv::cvtColor(new_cv_mat, display_cloud,)
+
+    //cv::imshow("PointCloudMap", new_cv_mat);
+
+    if (i == 0) {
+        SaveMatBinary("ExampleMatrix_MultipleBlock", new_cv_mat);
+        i++;
+    }
+
+}
+
 
 //ADD PCL Library
 //CREATE FUNCTION TO TRANSLATE PointCloud2 TO CV::Mat
 
+#define SUBSCRIBER 1
+
+void print_matrix (cv::Mat matrice) {
+    for (int h = 0; h < 1; h++) {
+        for (int w = 0; w < 512; w++) {
+            cv::Vec4f point = matrice.at<cv::Vec4f>(h,w);
+            std::cout << "Vx: " << point[0] << " Vy: " << point[1] << " Vz: " << point[2] << " Vd: " << point[3] << std::endl;
+        }
+    }
+}
+
 int main (int argc, char **argv) {
     
-    ros::init(argc, argv, "CPP_Listener");
-    ros::NodeHandle ros_handle;
-    ros::Subscriber ros_subscriber = ros_handle.subscribe("chatter", 1000, ros_callback);
+    if (SUBSCRIBER) {
+        ros::init(argc, argv, "CPP_Listener");
+        ros::NodeHandle ros_handle;
+        ros::Subscriber ros_subscriber = ros_handle.subscribe("chatter", 1000, ros_callback);
 
-    ros::spin();
+        ros::spin();
+    } else {
+        cv::Mat pcm(1, 512, CV_32FC4, cv::Scalar(0));
+        LoadMatBinary("TestMatrixBinary", pcm);
+        print_matrix(pcm);
+    }
+    
     
     return 0;
 
