ada525195da327abd6e0bbb0cae385399a522317
Dawwo20415 <davidemongera@hotmail.com>
2023-02-18 12:44:25 +0100

    Added check for point inside detection

--- computer_vision/src/libraries/location_handler.cpp
+++ computer_vision/src/libraries/location_handler.cpp
@@ -27,10 +27,30 @@ cv::Vec4f LocationHandler::selectDetection(std::vector<cv::Vec4f> detections, in
     return detections[selected];
 }
 
-cv::Vec3f LocationHandler::extrapolateDetectionPosition(cv::Vec4f selected, cv::InputArray point_cloud_array) {
+cv::Vec3f LocationHandler::extrapolateDetectionPosition(cv::Vec4f selected, cv::InputArray point_cloud_array, cv::Size2i template_size) {
+    //Make the Vec4f into a Contour
     //Determine if a point is inside the selection
         //Would probably be better to get all the points in a separate array but then I would still need to iterate trough them
     //Add the thing to the karis average
         //This needs to be done for each of the components like x,y,z 
     //return the result of the karis average
+
+    cv::RotatedRect rect = cv::RotatedRect (cv::Point2f(selected[0], selected[1]), 
+                                            cv::Size2f(template_size.width * selected[2], template_size.height * selected[2]), 
+                                            selected[3]);
+    cv::Point2f corners[4];
+    rect.points(corners);
+
+    //Converting array to a vector
+    cv::Point2f* lastItemPointer = (corners + sizeof corners / sizeof corners[0]);
+    std::vector<cv::Point2f> contour(corners, lastItemPointer);
+
+
+    for (int r = 0; r < point_cloud_array.rows(); r++) {
+        for (int c = 0; c < point_cloud_array.cols(); c++) {
+            if (!cv::pointPolygonTest(contour, cv::Point2f((float)c,(float)r), false)) {continue;}
+            //Add to the average
+
+        }
+    }
 }
--- computer_vision/src/libraries/location_handler.h
+++ computer_vision/src/libraries/location_handler.h
@@ -6,7 +6,7 @@
 namespace LocationHandler {
 
     cv::Vec4f selectDetection(std::vector<cv::Vec4f> detections, int width, int height);
-    cv::Vec3f extrapolateDetectionPosition(cv::Vec4f selected, cv::InputArray point_cloud_array);
+    cv::Vec3f extrapolateDetectionPosition(cv::Vec4f selected, cv::InputArray point_cloud_array, cv::Size2d template_size);
 
 }
 
