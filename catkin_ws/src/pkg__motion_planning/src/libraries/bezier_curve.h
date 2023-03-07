6d7f95f8996538a878b1e8e3590989bb125c68d9
Dawwo20415 <davidemongera@hotmail.com>
2023-03-07 14:46:29 +0100

    Settin up for motion MotionPlanning_Node

--- catkin_ws/src/pkg__motion_planning/src/libraries/bezier_curve.h
+++ catkin_ws/src/pkg__motion_planning/src/libraries/bezier_curve.h
@@ -1,6 +1,25 @@
 #ifndef __BEZIER_H__
 #define __BEZIER_H__
 
+#include <vector>
+#include <math.h>
 
+namespace Bezier {
+
+    typedef std::vector<float> Node;
+    typedef std::vector<Node> NodeVector;
+
+    class Curve {
+        private:
+            NodeVector nodes;
+            int segment_quantity;
+            int step;
+
+            Node calculate(float t);
+        public:
+            Curve(/* args */);
+    };
+
+}
 
 #endif
