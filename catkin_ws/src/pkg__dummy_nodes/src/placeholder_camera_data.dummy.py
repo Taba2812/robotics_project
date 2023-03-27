ccf88e17b628f89e0c59f0b7392c744103416d5d
Dawwo20415 <davidemongera@hotmail.com>
2023-03-27 16:06:23 +0200

    Catkin sends correct path string

--- catkin_ws/src/pkg__dummy_nodes/src/placeholder_camera_data.dummy.py
+++ catkin_ws/src/pkg__dummy_nodes/src/placeholder_camera_data.dummy.py
@@ -2,14 +2,16 @@
 
 import rospy
 import cv2
+import os
 
 from sensor_msgs import point_cloud2
 from sensor_msgs.msg import PointCloud2, PointField
 from std_msgs.msg import Header
 from std_msgs.msg import Bool, String
 
-point_cloud_file_path = "hello/path"
-image_mat_file_path = "hello/pat/2"
+src_path = os.getcwd() + "/src/images_database/complete_data_examples/"
+point_cloud_file_path = src_path + "Example_Pcl_SingleBlock.png"
+image_mat_file_path = src_path + "Example_Image_Color_SingleBlock.png"
 
 def generate_data():
     image = cv2.imread(image_mat_file_path)
@@ -54,10 +56,12 @@ def fake_data_callback(data):
 
 
 if __name__ == '__main__':
-    try:
-        rospy.init_node('Camera_Dummy')
+    
+    print(image_mat_file_path)
+    #try:
+    #    rospy.init_node('Camera_Dummy')
         
-        sub = rospy.Subscriber('Camera_Request', Bool, callback=fake_data_callback)
-        rospy.spin()
-    except rospy.ROSInterruptException:
-        pass
+    #    sub = rospy.Subscriber('Camera_Request', Bool, callback=fake_data_callback)
+    #    rospy.spin()
+    #except rospy.ROSInterruptException:
+    #    pass
