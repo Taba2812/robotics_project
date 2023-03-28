ce896a451517d7e437016bddf0254c9bacc95f95
Dawwo20415 <davidemongera@hotmail.com>
2023-03-28 15:06:50 +0200

    Camera dummy can generate placeholder images

--- catkin_ws/src/pkg__dummy_nodes/src/placeholder_camera_data.dummy.py
+++ catkin_ws/src/pkg__dummy_nodes/src/placeholder_camera_data.dummy.py
@@ -1,5 +1,6 @@
 #!usr/bin/env python
 
+import numpy as np
 import rospy
 import cv2
 import os
@@ -13,9 +14,20 @@ src_path = os.getcwd() + "/src/images_database/complete_data_examples/"
 point_cloud_file_path = src_path + "Example_Pcl_SingleBlock.png"
 image_mat_file_path = src_path + "Example_Image_Color_SingleBlock.png"
 
-def generate_data():
+def file_data():
     image = cv2.imread(image_mat_file_path)
     point_cloud = cv2.imread(point_cloud_file_path)
+
+    return point_cloud, image
+
+def generate_data():
+    image = cv2.imread(image_mat_file_path)
+    point_cloud = np.zeros((image.shape[0], image.shape[1], 3), dtype=np.float32)
+
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            v = (i+j)/2/(image.shape[0]+image.shape[1])
+            point_cloud[i,j] = [v,v,v]
 
     return point_cloud, image
 
@@ -45,6 +57,8 @@ def elaborate_point_cloud(point_cloud, image):
     return to_return
 
 def fake_data_callback(data):
+    if (not (data.data)):
+        return
     data_pub = rospy.Publisher('Camera_Data', PointCloud2, queue_size=10)
     imag_pub = rospy.Publisher('Image_String', String, queue_size=10)
 
@@ -53,15 +67,23 @@ def fake_data_callback(data):
     data_pub.publish(result)
     imag_pub.publish(image_mat_file_path)
 
+def display_data():
+    print(2)
 
+    point_cloud, image = generate_data()
+    cv2.imshow("point_cloud", point_cloud)
+    cv2.imshow("image", image)
+
+    cv2.waitKey(0)
+    # and finally destroy/close all open windows
+    cv2.destroyAllWindows()
 
 if __name__ == '__main__':
-    
-    print(image_mat_file_path)
-    #try:
-    #    rospy.init_node('Camera_Dummy')
+    try:
+        rospy.init_node('Camera_Dummy')
         
-    #    sub = rospy.Subscriber('Camera_Request', Bool, callback=fake_data_callback)
-    #    rospy.spin()
-    #except rospy.ROSInterruptException:
-    #    pass
+        sub = rospy.Subscriber('Camera_Request', Bool, callback=fake_data_callback)
+        rospy.spin()
+        #display_data()
+    except rospy.ROSInterruptException:
+        pass
