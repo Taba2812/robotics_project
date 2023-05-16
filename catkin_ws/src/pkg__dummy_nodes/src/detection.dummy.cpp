b4436f537573b4fe1eddea7b730a1026e709383c
Dawwo20415 <davidemongera@hotmail.com>
2023-05-16 17:03:23 +0200

    Refactored dummy node for new launch files and deleted unused and/or redundant files

--- catkin_ws/src/pkg__dummy_nodes/src/detection.dummy.cpp
+++ catkin_ws/src/pkg__dummy_nodes/src/detection.dummy.cpp
@@ -5,20 +5,26 @@
 #define RATIO 1000
 
 int main (int argc, char **argv) {
-    ros::init(argc, argv, "Detection_Dummy");
+    ros::init(argc, argv, "Core-Detection_Dummy");
 
     ros::NodeHandle handle;
 
+    //setup parameters
+    std::string det_req, det_res;
+    int queue;
+    handle.getParam("Core2Det_Req", det_req);
+    handle.getParam("Det2Core_Res", det_res);
+    handle.getParam("Q_Size", queue);
     
-    ros::Publisher pub = handle.advertise<std_msgs::Bool>("Detection_Request", RATIO);
+    ros::Publisher pub = handle.advertise<std_msgs::Bool>(det_req, queue);
 
     auto detection_callback = [&] (const std_msgs::Float32MultiArrayConstPtr &result) {
-        std::cout <<  "Block Detected at position: [" << result->data[0] << "," << result->data[1] << "," << result->data[2] << "]" << std::endl;
+        std::cout <<  "[Core][Dummy] Block Detected at position: [" << result->data[0] << "," << result->data[1] << "," << result->data[2] << "]" << std::endl;
     };
 
-    ros::Subscriber sub = handle.subscribe<std_msgs::Float32MultiArray>("Detection_Result", RATIO, detection_callback);
+    ros::Subscriber sub = handle.subscribe<std_msgs::Float32MultiArray>(det_res, queue, detection_callback);
 
-    std::cout << "Press any key ";
+    std::cout << "[Core][Dummy] Press any key to send a detection request: ";
     std::cin.get();
 
     std::cout << "[Core][Dummy] Sending Detection Request" << std::endl;
